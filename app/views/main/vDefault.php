bf2e8823526e2950f0496b982c983889ac904c98
Antoine Burnel <burnel.antoine@gmail.com>
2015-10-08 11:19:36 +0200

    dzdza

--- app/views/main/vDefault.php
+++ app/views/main/vDefault.php
@@ -15,7 +15,8 @@
 			
 				<a class="btn btn-default" href="users">Utilisateurs</a>
 				<a class="btn btn-primary" href="categories">Catégories</a>
-				<a class="btn btn-info" href="tickets">Tickets</a>					
+				<a class="btn btn-info" href="tickets">Tickets</a>	
+					<a class="btn btn-info" href="tickets">lol</a>				
 				<a class="btn btn-success" href="statuts">Statuts</a>
 				<a class="btn btn-warning" href="faqs">Faq</a>
 				<a class="btn btn-danger" href="messages">Messages</a>
