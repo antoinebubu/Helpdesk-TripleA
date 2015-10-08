84bfc0b62eddcc07092c23643ea6ebb250ee70ce
aclavel <antoine.clavel@sts-sio-caen.info>
2015-10-08 11:47:38 +0200

    oklm

--- app/views/main/vFooter.php
+++ app/views/main/vFooter.php
@@ -2,5 +2,5 @@
 <div style="height:100px; background-color:#C7EAEB; display:block; text-align:center; padding-top:20px;" >
 <p>By Apolline MALLET & Antoine BURNEL & Antoine CLAVEL</p>
 <br>
-<p>TEAM TRIPLE A</p>
+<p>TEAM AAA</p>
 </div>
