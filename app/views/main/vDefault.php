bf224f1e0a6fe7c0d5154a69ee30fd515fe141ef
apolline.mallet <apolline.mallet@sts-sio-caen.info>
2015-10-08 10:57:21 +0200

    Revert "ultime test"
    This reverts commit a3f889567b8f4d6a7920f688a9b2a9deb07a9d90.

--- app/views/main/vDefault.php
+++ app/views/main/vDefault.php
@@ -15,7 +15,10 @@
 			
 				<a class="btn btn-default" href="users">Utilisateurs</a>
 				<a class="btn btn-primary" href="categories">Catégories</a>
-				<a class="btn btn-info" href="tickets">Tickets</a>					
+				<a class="btn btn-info" href="tickets">Tickets</a>
+				<a class="btn btn-info" href="coucou">coucou</a>	
+				<a class="btn btn-info" href="coucou">coucou</a>	
+				
 				<a class="btn btn-success" href="statuts">Statuts</a>
 				<a class="btn btn-warning" href="faqs">Faq</a>
 				<a class="btn btn-danger" href="messages">Messages</a>
