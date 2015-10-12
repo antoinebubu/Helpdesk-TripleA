5a556bf3fe4a865b4b1c781919725a015cdf9c67
Antoine Burnel <burnel.antoine@gmail.com>
2015-10-12 09:04:23 +0200

    test
    dzdz

--- tests/NouveauxTicketsTest.php
+++ tests/NouveauxTicketsTest.php
@@ -4,10 +4,15 @@ class FirstTest extends \PHPUnit_Framework_TestCase {
 	
 	
 	
-public function testNouveauxTickets(){
-	$ticket=DAO::getOne("ticket", 1);
-	global $config;
+// public function testNouveauxTickets(){
+// 	$ticket=DAO::getOne("ticket", 1);
+// 	global $config;
 	
-	$this->assertEquals($notif, 1);
-}
+// 	$this->assertEquals($notif, 1);
+// }
+
+	public function testOne(){
+	
+		$this->assertEquals(3, 2+1);
+	}
 }
