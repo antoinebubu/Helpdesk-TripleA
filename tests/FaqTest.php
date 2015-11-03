2b59c894d281101462767dd1a2c7bbb57ad07045
aclavel <antoine.clavel@sts-sio-caen.info>
2015-11-03 16:00:45 +0100

    OKLMaOKLMOKLM

--- tests/FaqTest.php
+++ tests/FaqTest.php
@@ -11,14 +11,14 @@ class FaqTest extends AjaxUnitTest {
 		DAO::connect($config["database"]["dbName"]);
 	}
 	
-/*	public function testArticleFAQ(){
+	public function testArticleFAQ(){
 			
 		$faq=DAO::getOne("Faq", "5");
 		$this->assertEquals($faq->getId(), 5);
 	}
-*/
 
-/*	public function test_Bouton_Ajouter_Retour_Article_FAQ(){
+
+	public function test_Bouton_Ajouter_Retour_Article_FAQ(){
 		$this->get("DefaultC/asAdmin");
 		$this->waitFor(5);
 		$this->get("Faqs");
@@ -37,9 +37,9 @@ class FaqTest extends AjaxUnitTest {
 		$bt3=$this->getElementBySelector(".btn-ajouter");
 		$this->assertNotNull($bt3);
 	}
-*/
 
-/*	public function test_Ajouter_Article_FAQ(){
+
+	public function test_Ajouter_Article_FAQ(){
 		$this->get("DefaultC/asAdmin");
 		$this->waitFor(2);
 		$this->get("Faqs");
@@ -63,9 +63,9 @@ class FaqTest extends AjaxUnitTest {
 		$this->assertEquals($faqElt->getTitre(), "Titre de l'article");
 		$this->assertEquals($faqElt->getCategorie(), "Réseau");
 	}
-*/
-	
-/*	public function test_Visualisation_EtRetour_Article_FAQ(){
+
+
+	public function test_Visualisation_EtRetour_Article_FAQ(){
 		$this->get("Faqs");
 		$this->waitFor(2);
 		$this->assertPageContainsText("Foire aux questions");
@@ -81,9 +81,9 @@ class FaqTest extends AjaxUnitTest {
 		$bt->click();
 		$this->assertPageContainsText("Foire aux questions");
 	}
-	*/
+
 	
-/*	public function test_Modif_FAQ(){
+	public function test_Modif_FAQ(){
 		$this->get("DefaultC/asAdmin");
 		$this->waitFor(2);
 		$this->get("Faqs");
@@ -109,8 +109,9 @@ class FaqTest extends AjaxUnitTest {
 		$this->assertEquals($faqElt->getTitre(), "Titre de l'article 2");
 		$this->assertEquals($faqElt->getCategorie(), "Routage");
 	}
-	*/
-/*	public function test_Disable(){
+
+
+	public function test_Disable(){
 		$this->get("DefaultC/asAdmin");
 		$this->waitFor(2);
 		$this->get("Faqs");
@@ -130,9 +131,9 @@ class FaqTest extends AjaxUnitTest {
 		$faqElt=DAO::getOne("Faq", "1=1 order by dateCreation DESC LIMIT 0,1");
 		$this->assertEquals($faqElt->getDisable(), "0");
 	}
-	*/
 	
-/*	public function test_delete(){
+
+	public function test_delete(){
 		$this->get("DefaultC/asAdmin");
 		$this->waitFor(2);
 		$this->get("Faqs");
@@ -148,13 +149,39 @@ class FaqTest extends AjaxUnitTest {
 		$bt=$this->getElementBySelector(".glyphicon.glyphicon-remove");
 		$this->assertNotNull($bt);
 		$bt->click();
-		$this->waitFor(5);
-		$this->waitFor(5);
-		$this->waitFor(5);
-		$this->waitFor(5);
 		$queryy=DAO::$db->query("SELECT Count(*) FROM faq")->fetchColumn();
 		
 		$this->assertEquals($query, $queryy+1);
 	}
-	*/
+	
+	
+	public function test_admin_mes_articles_FAQ(){
+		$this->get("DefaultC/asAdmin");
+		$this->waitFor(2);
+		$this->get("Faqs");
+		$this->waitFor(2);
+		$this->assertPageContainsText("Foire aux questions");
+		
+		$bt=$this->getElementBySelector(".MesArticles");
+		$this->assertNotNull($bt);
+		$bt->click();
+		
+		$articles=DAO::getAll("Faq","iduser=1");
+		foreach ($articles as $article){
+			$this->assertPageContainsText($article->getTitre());
+		}
+		
+		$this->assertEquals(count($this->getElementsBySelector("td.titre-faq")),count($articles));
+	}
+
+	
+	public function test_Modif_Disable_Delete_SES_ARTICLES(){
+		$this->get("DefaultC/asAdmin");
+		$this->waitFor(2);
+		$this->get("Faqs");
+		$this->waitFor(2);
+		$this->assertPageContainsText("Foire aux questions");
+		$oklm=DAO::getAll("Faq","iduser=1");
+		$this->assertEquals(count($this->getElementsBySelector("td.x")),3*count($oklm));
+	}
 }
